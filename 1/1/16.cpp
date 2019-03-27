fff6856464161e37417287cc9a894d184ba7e4d2
Karim Salavatov <karimsalavatov100@gmail.com>
2019-03-27 16:10:44 +0300

    16 задание

---
+++ 1/1/16.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	double r,s,l;
+	const double pi=3.14;
+	cin >> l;
+	r=l/(2*pi);
+	s=pi*r*r;
+	cout << s << endl;
+	system("pause");
+	return 0;
+}
