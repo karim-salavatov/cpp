1a5bba4b83b6bd1ca6a721867e75af5be1cd2a7c
Karim Salavatov <karimsalavatov100@gmail.com>
2019-03-27 16:24:56 +0300

    17 задание

---
+++ 1/1/17.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	double r1,r2,s;
+	cin >> r2 >> r1;
+	const double pi=3.14; 
+	s=pi*(r2*r2-r1*r1);
+	cout << s <<  endl; 
+	system("pause");
+	return 0;
+}
