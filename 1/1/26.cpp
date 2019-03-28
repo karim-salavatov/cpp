3a7b2661c7ee3b9dac36477a1bd6abb183468b38
Karim Salavatov <karimsalavatov100@gmail.com>
2019-03-28 12:01:28 +0300

    26 задание

---
+++ 1/1/26.cpp
@@ -0,0 +1,17 @@
+#include<cmath>
+#include<iostream>
+using namespace std;
+int main()
+{
+	setlocale(0LL,"rus");
+   double s,phi,R;
+   cout <<" ¬ведите r:";
+   cin>>R;
+  cout << "¬ведите phi =";
+  cin >> phi;
+   s=(phi*R*R)/2;
+   cout <<"ѕлощадь = " << " " <<s<<endl;
+   system("pause");
+    return 0;
+}
+
