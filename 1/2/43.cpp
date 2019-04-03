4b1ec2a31d1cf93e9f09994432cad2fd12dd1b32
Karim Salavatov <karimsalavatov100@gmail.com>
2019-04-03 18:37:11 +0400

    43

---
+++ 1/2/43.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	double a,b,c;
+	cin >> a >> b >> c;
+	if(a>0) cout << a*a << " ";
+	if(b>0) cout << b*b << " ";
+	if(c>0) cout << c*c << " ";
+	return 0;
+}
